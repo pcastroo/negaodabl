e2c175cab871f5651e7c8a7cdba92824e12c0398
pcastroo <pedroddcastro21@gmail.com>
2020-07-18 22:10:45 -0300

    add esp/ font

--- js/index.js
+++ js/index.js
@@ -6,7 +6,8 @@ function playAudio(type) {
 		"loli": `./audio/loli.mp3`,
 		"japanese": `./audio/japones.mp3`,
 		"english": `./audio/negao-da-bl-em-ingles.mp3`,
-		"russian": `./audio/savetweetvid_zwidN7WQvX696RY7.mp3`
+		"russian": `./audio/savetweetvid_zwidN7WQvX696RY7.mp3`,
+		"espanhol": `./audio/savetweetvid_Zz_484Rw_tgzlzH8.mp3`
 	};
 	const audio = new Audio();
 	audio.src = audios[type];
