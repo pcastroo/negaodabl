59dc1b28b7f22f1e875ca3fc6cea9212d3230cbd
pcastroo <pedroddcastro21@gmail.com>
2020-07-24 12:06:42 -0300

    add frances/pt lang

--- js/index.js
+++ js/index.js
@@ -7,7 +7,8 @@ function playAudio(type) {
 		"japanese": `./audio/japones.mp3`,
 		"english": `./audio/negao-da-bl-em-ingles.mp3`,
 		"russian": `./audio/savetweetvid_zwidN7WQvX696RY7.mp3`,
-		"espanhol": `./audio/savetweetvid_Zz_484Rw_tgzlzH8.mp3`
+		"espanhol": `./audio/savetweetvid_Zz_484Rw_tgzlzH8.mp3`,
+		"franceis": `./audio/franceis.mp3`
 	};
 	const audio = new Audio();
 	audio.src = audios[type];
